985f160bf0c6e95b69d9e45178b1d006f8ccb27a
Hashem <hashemabualteen@gmail.com>
2023-01-29 23:17:56 +0200

    Initial commit
    Implemented two APIs , that use gpt 3 from openai to perform actions on text.

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const { Configuration, OpenAIApi } = require("openai");
+
+const app = express();
+
+const configuration = new Configuration({
+    apiKey: myOpenAiKey,
+});
+const openai = new OpenAIApi(configuration);
+
+
+app.use(express.json());
+
+app.post('/ai/review-and-rephrase', async(req, res) => {
+  const data = req.body;
+  const text = "review and rephrase:\n"+data.text;
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: text,
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+  res.status(200).send(response.data.choices[0].text);
+});
+
+// generic promt to generate text 
+app.post('/generic-completion/:promt', async(req , res) => {
+    const data = req.body;
+    const action = req.params.promt;
+    if(!req.body.text){
+        res.status(400).send("there is no text");
+    }
+    const text = action +"\n" + req.body.text;
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: text,
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      });
+      res.status(200).send(response.data.choices[0].text);
+})
+
+app.listen(3000, () => console.log('Server started on port 3000'));
