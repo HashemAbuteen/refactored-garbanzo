27ea3ca2c5ea3d165efda2522eeabb01f791511c
Hashem <hashemabualteen@gmail.com>
2023-02-07 17:16:24 +0200

    add more error handling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const { Configuration, OpenAIApi } = require("openai");
 
@@ -9,42 +9,46 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-
 app.use(express.json());
-
-app.post('/ai/review-and-rephrase', async(req, res) => {
-  const data = req.body;
-  const text = "review and rephrase:\n"+data.text;
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: text,
-    temperature: 0.7,
-    max_tokens: 256,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-  });
-  res.status(200).send(response.data.choices[0].text);
-});
 
 // generic promt to generate text 
 app.post('/generic-completion/:promt', async(req , res) => {
     const data = req.body;
     const action = req.params.promt;
+
     if(!req.body.text){
-        res.status(400).send("there is no text");
+        console.log("No text was provided in the request body");
+        return res.sendStatus(400);
     }
+
+    if(!process.env.OPENAI_API_KEY) {
+        console.log("The environment variable for OpenAI API key is not set");
+        return res.sendStatus(200);
+    }
+
     const text = action +": \n" + req.body.text;
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: text,
-        temperature: 0.7,
-        max_tokens: 256,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-      });
-      res.status(200).send(response.data.choices[0].text);
-})
+
+    try {
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: text,
+            temperature: 0.7,
+            max_tokens: 256,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        });
+
+        if(!response.data.choices[0].text) {
+            console.log("The response from OpenAI does not contain the 'text' property");
+            return res.sendStatus(500);
+        }
+
+        res.status(200).send(response.data.choices[0].text);
+    } catch (error) {
+        console.log(`An error occurred while calling the OpenAI API: ${error}`);
+        return res.sendStatus(500);
+    }
+});
 
 app.listen(3000, () => console.log('Server started on port 3000'));
